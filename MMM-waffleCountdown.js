e813249af5b502bbc3ba0caa1725b3b6604e1097
Sondre Vestad <sonves@gmail.com>
2017-10-27 16:56:37 +0200

    cleaned up some code

--- MMM-waffleCountdown.js
+++ MMM-waffleCountdown.js
@@ -1,9 +1,119 @@
 
-let dayOfWeek = 1; // 1 for monday
-let timeOfDay = 13; //END time
+let dayOfWeek = 4; // 1 for monday
+let timeOfDay = 21+1; //END time
 
-let pad = function(value) {
-  if (value<10) return '0'+value; else return value
+let overlayIsOn=false
+let globalOverlay=undefined
+
+let pad = function(value) {if (value<10) return '0'+value; else return value}
+
+let overlayOn = function() {
+  if (!overlayIsOn) {
+    var overlay = document.createElement("div");
+    overlay.id = "overlay";
+    overlay.innerHTML += "<div class=\"black_overlay\"></div>";
+    document.body.insertBefore(overlay, document.body.firstChild);
+
+
+    globalOverlay = document.createElement("div");
+    globalOverlay.className= "fullscreenApp"
+    overlay.appendChild(globalOverlay)
+
+  }
+  overlayIsOn=true
+}
+
+let overlayOff = function() {
+  if (overlayIsOn) {
+    var overlay = document.getElementById("overlay");
+		overlay.parentNode.removeChild(overlay);
+  }
+  overlayIsOn=false
+  globalOverlay=undefined
+}
+
+let displayWaffleEnd = function() {
+  var wrapper = document.createElement("div");
+  var headerD = document.createElement("span");
+  var timeLeft = document.createElement("span");
+
+  timeLeft.innerHTML = "No more waffles!";
+  timeLeft.className = "doooom";
+
+  wrapper.appendChild(headerD);
+  wrapper.appendChild(timeLeft);
+  return wrapper;
+}
+
+let displayWaffleCountdown = function() {
+  var nowDate = new Date();
+  var startDate = new Date();
+  if (startDate.getDay()!=dayOfWeek) {
+    startDate.setDate(startDate.getDate() + (7+dayOfWeek-startDate.getDay()%7)  );
+  } else {
+    startDate.setDate(startDate.getDate());
+  }
+
+  startDate.setHours(timeOfDay)
+  startDate.setMinutes(0)
+  startDate.setSeconds(0)
+
+  var timeparser = Date.parse(startDate) - Date.parse(nowDate);
+  daysLeft = Math.floor(timeparser/(1000*60*60*24));
+  hoursLeft= Math.floor(timeparser/(1000*60*60));
+  minsLeft= Math.floor(timeparser/(1000*60));
+  secsLeft= Math.floor(timeparser/(1000));
+
+
+  var wrapper = document.createElement("div");
+  var headerD = document.createElement("span");
+  var timeLeft = document.createElement("span");
+
+  headerD.innerHTML = "Waffles in:" + "<br/><br/>";
+  headerD.className = "doooom";
+  var timeLeft = document.createElement("span")
+  timeLeft.innerHTML = pad(daysLeft) + ':' + pad(hoursLeft % 24) + ':' + pad(minsLeft % 60) + ':' + pad(secsLeft % 60)
+  timeLeft.className = "timeLeft";
+
+  wrapper.appendChild(headerD);
+  wrapper.appendChild(timeLeft);
+  return wrapper;
+}
+
+let displayWaffleFullscreen = function(timeLeft) {
+
+//overlayOn()
+//globalOverlay.innerHTML="BLABLABLABLA"
+
+var wrapper = document.createElement("div");
+var headerD = document.createElement("span");
+headerD.innerHTML = "Waffle ready!";
+var span = document.createElement("span");
+var barLength=20
+var stekeTid=180 //2:50
+var venteTid=30 //20sec
+var totaltTid=stekeTid+venteTid
+let now = new Date()
+var loadingTimeNow = Math.round(now.getTime() / 1000) % totaltTid
+
+if (loadingTimeNow<stekeTid) {
+  headerD.innerHTML = "Loading waffle:" + "<br/><br />";
+  var steps=Math.round(barLength*(loadingTimeNow/stekeTid))
+  var unfinished=barLength
+  var loadingBar = ""
+  for (var i=0;i<steps;i++) {
+    loadingBar+='#'
+    unfinished-=1
+  }
+  for (var i=0;i<unfinished;i++) {
+    loadingBar+='_'
+  }
+  headerD.innerHTML += loadingBar
+}
+wrapper.appendChild(headerD);
+wrapper.appendChild(span);
+return wrapper;
+
 }
 
 
@@ -16,92 +126,45 @@ Module.register("MMM-waffleCountdown", {
 
         setInterval(function() {
             self.updateDom();
-        }, 1000); //Run every sec
+            //self.sendNotification("SHOW_ALERT", {title:'TEST',message:'Woooooohoo'});
+        }, 1000);
     },
 
-    // Define required styles
     getStyles: function () {
         return ["MMM-waffleCountdown.css"];
     },
 
-    // Override dom generator.
     getDom: function() {
-        var now = new Date();
-        //var d = new Date();
 
-        var d = new Date();
-        if (d.getDay()!=dayOfWeek) {
-          d.setDate(d.getDate() + (7+dayOfWeek-d.getDay()%7)  );
+        var nowDate = new Date();
+        var startDate = new Date();
+        if (startDate.getDay()!=dayOfWeek) {
+          startDate.setDate(startDate.getDate() + (7+dayOfWeek-startDate.getDay()%7)  );
         } else {
-          d.setDate(d.getDate());
+          startDate.setDate(startDate.getDate());
         }
 
-        d.setHours(timeOfDay)
-        d.setMinutes(0)
-        d.setSeconds(0)
+        startDate.setHours(timeOfDay)
+        startDate.setMinutes(0)
+        startDate.setSeconds(0)
 
-        //var d = new Date().getNextWkDay(dayOfWeek);
-        //d.setHours(16);
-
-        var timeparser = Date.parse(d) - Date.parse(now);
-        daysLeft = Math.floor(timeparser/(1000*60*60*24));
-        hoursLeft= Math.floor(timeparser/(1000*60*60));
-        minsLeft= Math.floor(timeparser/(1000*60));
-        secsLeft= Math.floor(timeparser/(1000));
-
-        var wrapper = document.createElement("div");
-        var headerD = document.createElement("span");
-
-
-        if (daysLeft <= 1) {
-          if (daysLeft<=0 && secsLeft<0) {
-            var timeLeft = document.createElement("span");
-            timeLeft.innerHTML = "No more waffles!";
-            timeLeft.className = "doooom";
-          } else if (daysLeft==0 && secsLeft<=2*60*60) {
-            headerD.innerHTML = "Waffle ready!";
-            var timeLeft = document.createElement("span");
-            var barLength=20
-            var stekeTid=180 //2:50
-            var venteTid=30 //20sec
-            var totaltTid=stekeTid+venteTid
-            var loadingTimeNow = Math.round(now.getTime() / 1000) % totaltTid
-            console.log("t:"+loadingTimeNow)
-
-            if (loadingTimeNow<stekeTid) {
-              headerD.innerHTML = "Loading waffle:" + "<br/><br />";
-              var steps=Math.round(barLength*(loadingTimeNow/stekeTid))
-              console.log("s:"+steps)
-              var unfinished=barLength
-              var loadingBar = ""
-              for (var i=0;i<steps;i++) {
-                loadingBar+='#'
-                unfinished-=1
-              }
-              for (var i=0;i<unfinished;i++) {
-                loadingBar+='_'
-              }
-              headerD.innerHTML += loadingBar
-            }
-
-            headerD.className = "doooom";
-          } else {
-            headerD.innerHTML = "Waffles in:" + "<br/><br />";
-            headerD.className = "doooom";
-            var timeLeft = document.createElement("span")
-            timeLeft.innerHTML = pad((hoursLeft)%24) + ':' + pad(minsLeft % 60) + ':' + pad(secsLeft % 60)
-            timeLeft.className = "timeLeft";
-          }
-        } else {
-            headerD.innerHTML = "Waffles in:" + "<br/><br/>";
-            headerD.className = "doooom";
-            var timeLeft = document.createElement("span")
-            timeLeft.innerHTML = pad(daysLeft) + ':' + pad(hoursLeft % 24) + ':' + pad(minsLeft % 60) + ':' + pad(secsLeft % 60)
-            timeLeft.className = "timeLeft";
+        var timeparser = Date.parse(startDate) - Date.parse(nowDate);
+        var timeLeft =
+        {
+          days: Math.floor(timeparser/(1000*60*60*24)),
+          hours: Math.floor(timeparser/(1000*60*60)),
+          mins: Math.floor(timeparser/(1000*60)),
+          secs: Math.floor(timeparser/(1000))
         }
 
-        wrapper.appendChild(headerD);
-        wrapper.appendChild(timeLeft);
-        return wrapper;
+        if (timeLeft.days<=0 && timeLeft.hours<-2) {
+          return displayWaffleEnd();
+        } else if (timeLeft.days<=0 && timeLeft.secs<0) {
+          return displayWaffleFullscreen(timeLeft);
+        } else {
+          return displayWaffleCountdown();
+        }
+
+
     }
 });
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MMM-doomsDay
 
-This module for the [MagicMirror](https://github.com/MichMich/MagicMirror) shows the number of days left to a specific date.
+This module for the [MagicMirror](https://github.com/MichMich/MagicMirror) shows the time left until waffleMondays
 
 ## Installation
 
@@ -8,40 +8,14 @@ This module for the [MagicMirror](https://github.com/MichMich/MagicMirror) shows
 
 ```bash
 cd ~/MagicMirror/modules # navigate to module folder
-git clone https://github.com/brobergp/MMM-doomsDay.git # clone this repository
+git clone https://github.com/sondrev/MMM-waffleCountdown.git # clone this repository
 ```
 
   2\. Then, add the following into the `modules` section of your `config/config.js` file:
 
 ````javascript
 {
-    module: 'MMM-doomsDay',
-    position: 'top_center', // This can be any of the regions, best results in center regions
-    config: {
-        doomsDay: "2018-02-23 24:00:00", // YYYY-MM-DD HH:MM:SS, Do not alter the time, just the date
-        toWhat: "Leaving for Paris!",
-        
-        // See 'Configuration options' for more information.
-    }
+      module: 'MMM-waffleCountdown',
+      position: 'lower_third', // This can be any of the regions, best results in center regions
 },
 ````
-
-
-## customization
-
-  Copy the code from the MMM-doomsDay.css to your own custom.css and change whatever you like about the font, color etc.
-  Using the configuration options singular and plural you can change the language of day/days left.
-
-## Configuration options
-
-The following properties can be configured:
-
-| option | description |
-| ------------- | ------------- |
-| `updateInterval` | time between updates in ms, default is `60 * 60 * 1000` (1 hour) |
-| `toWhat` | the title of your countdown event |
-| `singular` | what it should say when it's only one day left, default is `Day Left` |
-| `plural` | what it should say when it's more days left, default is `Days Left` |
-| `doomsDay` | date and time as a string, the end day of the countdown, format is `YYYY-MM-DD HH:MM:SS`, default is `"2018-02-23 24:00:00"`, don't alter the hour, keep it at `24:00:00` |
-| `present` | What it says when the day is finally here! |
-| `timesUp`| What it says when the day has went and gone, default is `death and despair, your time is up.` |
