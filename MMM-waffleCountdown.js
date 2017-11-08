a13b7f55307a63940a57ad1ac6563b1339142877
Sondre Vestad <sonves@gmail.com>
2017-11-08 10:58:48 +0100

    cleaned up code

--- MMM-waffleCountdown.js
+++ MMM-waffleCountdown.js
@@ -1,130 +1,118 @@
-let dayOfWeek = 1; // 1 for monday
-let timeOfDay = 14; //star time.
-let minuteOfDay = 0; //star time.
-
-let overlayIsOn=false
-let globalOverlay=undefined
-
-let pad = function(value) {if (value<10) return '0'+value; else return value}
-
-let overlayOn = function() {
-  var overlayBlack = document.getElementById("overlayBlack");
-  if (overlayBlack!==undefined) {
-    overlayBlack.innerHTML = "<div class=\"black_overlay\"></div>";
-  }
-}
-
-let overlayOn2 = function(inner) {
-  var overlayBlack = document.getElementById("overlayBlack");
-  if (overlayBlack!==undefined) {
-    overlayBlack.innerHTML = "<div class=\"Aligner\"><div class=\"Aligner-item\">"+inner+"</div></div>";
-  }
-}
-
-let overlayOff = function() {
-  var overlayBlack = document.getElementById("overlayBlack");
-  if (overlayBlack!==undefined) {
-    overlayBlack.innerHTML="<div></div>"
-  }
-}
-
-let displayWaffleEnd = function() {
-  var wrapper = document.createElement("div");
-  var headerD = document.createElement("span");
-  var timeLeft = document.createElement("span");
-
-  timeLeft.innerHTML = "No more waffles!";
-  timeLeft.className = "text";
-
-  wrapper.appendChild(headerD);
-  wrapper.appendChild(timeLeft);
-  return wrapper;
-}
-
-let displayWaffleCountdown = function(timeLeft) {
-  var wrapper = document.createElement("div");
-  var headerD = document.createElement("span");
-  var timer = document.createElement("span");
-
-  headerD.innerHTML = "Waffles in:" + "<br/><br/>";
-  headerD.className = "text";
-  timer.innerHTML = pad(timeLeft.days) + ':' + pad(timeLeft.hours % 24) + ':' + pad(timeLeft.mins % 60) + ':' + pad(timeLeft.secs % 60)
-  timer.className = "timeLeft";
-
-  wrapper.appendChild(headerD);
-  wrapper.appendChild(timer);
-  return wrapper;
-}
-
-let displayWaffleCountdownBig = function(timeLeft) {
-  overlayOn()
-  var wrapper = document.createElement("div");
-  var headerD = document.createElement("span");
-  var timer = document.createElement("span");
-
-  timer.innerHTML = pad(timeLeft.days) + ':' + pad(timeLeft.hours % 24) + ':' + pad(timeLeft.mins % 60) + ':' + pad(timeLeft.secs % 60)
-  timer.className = "timeLeftBig";
-
-  //wrapper.appendChild(headerD);
-  //wrapper.appendChild(timer);
-  overlayOn2("<div class=\"timeLeftBig2\">"+timer.innerHTML+"</div>")
-  return wrapper;
-}
-
-let displayWaffleFullscreen = function(timeLeft) {
-  var wrapper = document.createElement("div");
-  var headerD = document.createElement("span");
-  headerD.innerHTML = "Waffle ready!";
-  headerD.className = "fullscreenApp"
-  var span = document.createElement("span");
-  var barLength=20
-  var stekeTid=170 //2:50
-  var venteTid=30 //20sec
-  var totaltTid=stekeTid+venteTid
-  let now = new Date()
-  var deltaTime = Math.floor((now.getTime() - timeLeft.startTime)/1000)
-  //var deltaTime=(timeLeft.startTime - Math.floor(now.getTime() / 1000))
-  var loadingTimeNow = (deltaTime) % totaltTid
-
-
-  if (loadingTimeNow<stekeTid) {
-    headerD.innerHTML = "Loading waffle:" + "<br/><br />";
-    var steps=Math.round(barLength*(loadingTimeNow/stekeTid))
-    var unfinished=barLength
-    var loadingBar = ""
-    for (var i=0;i<steps;i++) {
-      loadingBar+='#'
-      unfinished-=1
-    }
-    for (var i=0;i<unfinished;i++) {
-      loadingBar+='_'
-    }
-    headerD.innerHTML += loadingBar
-  }
-  //wrapper.appendChild(headerD);
-  //wrapper.appendChild(span);
-  overlayOn2("<div class=\"fullscreenApp2\">"+headerD.innerHTML+"</div>")
-  return wrapper;
-
-}
-
 
 Module.register("MMM-waffleCountdown", {
-    // Define start sequence.
+
+    defaults: {
+      dayOfWeek: 1, // 1 for monday
+      timeOfDay: 14,
+      minuteOfDay: 0
+    },
+
     start: function() {
         var self = this;
 
         Log.info("Starting module: " + this.name);
-
-        overlayBlack = document.createElement("div");
-        overlayBlack.id = "overlayBlack";
-        overlayBlack.innerHTML="<div></div>"
-        document.body.insertBefore(overlayBlack, document.body.firstChild);
-
+        this.waffleOverlayCreate();
         setInterval(function() {
             self.updateDom();
-            //self.sendNotification("SHOW_ALERT", {title:'TEST',message:'Woooooohoo'});
         }, 1000);
+    },
+
+    padTime: function(value) {if (value<10) return '0'+value; else return value},
+
+    waffleOverlayCreate: function(html) {
+      waffleOverlay = document.createElement("div");
+      waffleOverlay.id = "waffleOverlay";
+      waffleOverlay.innerHTML="<div></div>"
+      document.body.insertBefore(waffleOverlay, document.body.firstChild);
+    },
+
+    waffleOverlayUpdate: function(html) {
+      let waffleOverlay = document.getElementById("waffleOverlay");
+      if (waffleOverlay!==undefined) {
+        waffleOverlay.innerHTML = "<div class=\"Aligner\"><div class=\"Aligner-item\">"+html+"</div></div>";
+      }
+    },
+
+    waffleOverlayDisable: function() {
+      let waffleOverlay = document.getElementById("waffleOverlay");
+      if (waffleOverlay!==undefined) {
+        waffleOverlay.innerHTML="<div></div>"
+      }
+    },
+
+    displayWaffleEnd: function() {
+      var wrapper = document.createElement("div");
+      var spanText = document.createElement("span");
+
+      spanText.innerHTML = "No more waffles!";
+      spanText.className = "text";
+
+      wrapper.appendChild(spanText);
+      return wrapper;
+    },
+
+    displayWaffleCountdownBig: function(timeLeft) {
+      var wrapper = document.createElement("div");
+      var headerD = document.createElement("span");
+      var timer = document.createElement("span");
+
+      timer.innerHTML = this.padTime(timeLeft.days) + ':' + this.padTime(timeLeft.hours % 24) + ':' + this.padTime(timeLeft.mins % 60) + ':' + this.padTime(timeLeft.secs % 60)
+      timer.className = "timeLeftBig";
+
+      //wrapper.appendChild(headerD);
+      //wrapper.appendChild(timer);
+      this.waffleOverlayUpdate("<div class=\"timeLeftBig2\">"+timer.innerHTML+"</div>")
+      return wrapper;
+    },
+
+    displayWaffleFullscreen: function(timeLeft) {
+      var wrapper = document.createElement("div");
+      var headerD = document.createElement("span");
+      headerD.innerHTML = "Waffle ready!";
+      var span = document.createElement("span");
+      var barLength=20
+      var stekeTid=200 //2:50
+      var venteTid=50 //20sec
+      var totaltTid=stekeTid+venteTid
+      let now = new Date()
+      var deltaTime = Math.floor((now.getTime() - timeLeft.startTime)/1000)
+      //var deltaTime=(timeLeft.startTime - Math.floor(now.getTime() / 1000))
+      var loadingTimeNow = (deltaTime) % totaltTid
+
+
+      if (loadingTimeNow<stekeTid) {
+        headerD.innerHTML = "Loading waffle:" + "<br/><br />";
+        var steps=Math.round(barLength*(loadingTimeNow/stekeTid))
+        var unfinished=barLength
+        var loadingBar = ""
+        for (var i=0;i<steps;i++) {
+          loadingBar+='#'
+          unfinished-=1
+        }
+        for (var i=0;i<unfinished;i++) {
+          loadingBar+='_'
+        }
+        headerD.innerHTML += loadingBar
+      }
+
+      this.waffleOverlayUpdate("<div class=\"fullscreenApp2\">"+headerD.innerHTML+"</div>")
+      return wrapper;
+
+    },
+
+     displayWaffleCountdown: function(timeLeft) {
+      var wrapper = document.createElement("div");
+      var spanHeader = document.createElement("span");
+      var spanCountdown = document.createElement("span");
+
+      spanHeader.innerHTML = "Waffles in:" + "<br/><br/>";
+      spanHeader.className = "text";
+      spanCountdown.innerHTML = this.padTime(timeLeft.days) + ':' + this.padTime(timeLeft.hours % 24) + ':' + this.padTime(timeLeft.mins % 60) + ':' + this.padTime(timeLeft.secs % 60)
+      spanCountdown.className = "timeLeft";
+
+      wrapper.appendChild(spanHeader);
+      wrapper.appendChild(spanCountdown);
+      return wrapper;
     },
 
     getStyles: function () {
@@ -135,14 +123,14 @@ Module.register("MMM-waffleCountdown", {
 
         var nowDate = new Date();
         var startDate = new Date();
-        if (startDate.getDay()!=dayOfWeek) {
-          startDate.setDate(startDate.getDate() + (7+dayOfWeek-startDate.getDay()%7)  );
+        if (startDate.getDay()!=this.config.dayOfWeek) {
+          startDate.setDate(startDate.getDate() + (7+this.config.dayOfWeek-startDate.getDay()%7)  );
         } else {
           startDate.setDate(startDate.getDate());
         }
 
-        startDate.setHours(timeOfDay)
-        startDate.setMinutes(minuteOfDay)
+        startDate.setHours(this.config.timeOfDay)
+        startDate.setMinutes(this.config.minuteOfDay)
         startDate.setSeconds(0)
 
         var timeparser = Date.parse(startDate) - Date.parse(nowDate);
@@ -155,15 +143,15 @@ Module.register("MMM-waffleCountdown", {
           secs: Math.floor(timeparser/(1000))
         }
 
-        overlayOff()
+        this.waffleOverlayDisable()
         if (timeLeft.days<=0 && timeLeft.hours<-2) {
-          return displayWaffleEnd();
+          return this.displayWaffleEnd();
         } else if (timeLeft.days<=0 && timeLeft.secs<0) {
-          return displayWaffleFullscreen(timeLeft);
+          return this.displayWaffleFullscreen(timeLeft);
         } else if (timeLeft.days<=0 && timeLeft.secs<=10) {
-          return displayWaffleCountdownBig(timeLeft);
+          return this.displayWaffleCountdownBig(timeLeft);
         } else {
-          return displayWaffleCountdown(timeLeft);
+          return this.displayWaffleCountdown(timeLeft);
         }
 
 
